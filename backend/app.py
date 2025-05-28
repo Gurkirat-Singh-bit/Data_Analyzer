dd992b2b9e53eda4baebe5a88f0eed08d9013c31
Gurkirat-Singh-bit <mr.gurkirat.singh.no1@gmail.com>
2025-05-29 02:35:49 +0530

    Frontend part

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,10 @@
-# Flask or FastAPI server
+# Flask server  
+
+from flask import jsonify, Flask, request   # importing nessary libraries and modules
+import os
+
+app = Flask(__name__)
+
+Uploads = 'uploads'
+
+
